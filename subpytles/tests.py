8a26f4cd02954b40f29ef80e46e50674c40d3a56
Vincent Agnano <vincent.agnano@particul.es>
2012-10-04 00:37:01 +0200

    cleaned root __init__ and added delay test

--- subpytles/tests.py
+++ subpytles/tests.py
@@ -14,12 +14,13 @@ class SrtEditorTest(unittest.TestCase):
         self.editor = SrtEditor(content)
 
     def test_read_file(self):
-        self.assertEqual(self.editor.content, "1\r\n00:01:34,680 --> 00:01:35,874\r\nTu veux un ballon ?\r\n\r\n3\r\n00:01:38,440 --> 00:01:39,793\r\n5 euros\r\n\r\n4\r\n00:01:41,680 --> 00:01:42,635\r\nLe re\xe7u ?\r\n\r\n")
+        self.assertEqual(self.editor.content, "1\r\n00:01:34,680 --> 00:01:35,874\r\nTu veux un ballon ?\r\n\r\n2\r\n00:01:36,080 --> 00:01:38,036\r\n- Maman, tu me l'ach\xe8tes ?\r\n\r\n")
 
     def test_delay(self):
         self.editor.delay(1000)
-        self.assertEqual(self.editor.content, "1\r\n0:01:35,680 --> 0:01:36,874\r\nTu veux un ballon ?\r\n\r\n3\r\n0:01:39,440 --> 0:01:40,793\r\n5 euros\r\n\r\n4\r\n0:01:42,680 --> 0:01:43,635\r\nLe re\xe7u ?\r\n\r\n")
-
+        self.assertEqual(self.editor.content, "1\r\n0:01:35,680 --> 0:01:36,874\r\nTu veux un ballon ?\r\n\r\n2\r\n0:01:37,080 --> 0:01:39,036\r\n- Maman, tu me l'ach\xe8tes ?\r\n\r\n")
+        self.editor.delay(-1000)
+        self.assertEqual(self.editor.content, "1\r\n0:01:34,680 --> 0:01:35,874\r\nTu veux un ballon ?\r\n\r\n2\r\n0:01:36,080 --> 0:01:38,036\r\n- Maman, tu me l'ach\xe8tes ?\r\n\r\n")
 
 if __name__ == "__main__":
     unittest.main()
