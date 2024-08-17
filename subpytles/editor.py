95be873039cf181c4b617e56b4bb6034de914d8a
Vincent Agnano <vinyll@protonmail.com>
2024-08-17 17:14:35 +0200

    Fixed formatting with double digits

--- subpytles/editor.py
+++ subpytles/editor.py
@@ -6,6 +6,21 @@ from datetime import timedelta
 
 row_format = "%(id)s\r\n%(start)s --> %(end)s\r\n%(text)s\r\n\r\n"
 
+
+def format_timedelta(td):
+    # Get total seconds from timedelta
+    total_seconds = int(td.total_seconds())
+    
+    # Calculate hours, minutes, seconds, and milliseconds
+    hours, remainder = divmod(total_seconds, 3600)
+    minutes, seconds = divmod(remainder, 60)
+    milliseconds = int((td.total_seconds() - total_seconds) * 1000)
+
+    # Format the time string
+    formatted_time = f"{hours:02}:{minutes:02}:{seconds:02},{milliseconds:03}"
+    return formatted_time
+
+
 class SrtEditor(object):
 
     def __init__(self, content, *args, **kwargs):
@@ -25,10 +40,12 @@ class SrtEditor(object):
             text = text
             t = re.findall(time_pattern, start)[0]
             start_time = timedelta(hours=int(t[0]), minutes=int(t[1]), seconds=int(t[2]), milliseconds=int(t[3]))
-            start = str(start_time + timedelta(milliseconds=time)).replace('.', ',')[:-3]
+            start_delta = start_time + timedelta(milliseconds=time)
+            start = format_timedelta(start_delta)
             t = re.findall(time_pattern, end)[0]
             end_time = timedelta(hours=int(t[0]), minutes=int(t[1]), seconds=int(t[2]), milliseconds=int(t[3]))
-            end = str(end_time + timedelta(milliseconds=time)).replace('.', ',')[:-3]
+            end_delta = end_time + timedelta(milliseconds=time)
+            end = format_timedelta(end_delta)
             output.append((id, start, end, text))
         self.contents = output
 
